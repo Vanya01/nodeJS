b0ff8e9fe050521e5fd2b3245ef551299e4d7782
Vanya01 <good.boy.2k19@gmail.com>
2021-10-13 16:22:03 +0300

    Home_work-4_node_JS

--- Controllers/userLogIn.js
+++ Controllers/userLogIn.js
@@ -1,21 +1,9 @@
-const UsersHere = require('../dataBase/User');
-
 module.exports = {
     login: (req, res) => {
         try {
-            res.json('Done!');
+            res.json('Signed_In!');
         } catch (e) {
             res.json(e.message);
         }
     },
-
-    logout: async (req, res) => {
-        try {
-            const user = await UsersHere.findOne();
-
-            res.json(user);
-        } catch (err) {
-            res.json(err);
-        }
-    }
 };
